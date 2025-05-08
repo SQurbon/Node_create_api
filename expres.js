a97a3b2aa8a3e52b8e33c016dda45d5b9a5c974c
“SQurbon” <qurbonsultonqulov466@gmail.com>
2025-05-08 15:12:08 +0500

    new

--- expres.js
+++ expres.js
@@ -1,20 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const cors = require('cors'); // agar frontend ulanadigan bo‘lsa
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use(cors()); // frontenddan ulanish uchun
-app.use(express.static('public')); // public papkadan rasm yoki fayl xizmatini yoqish
+app.use(cors());
+app.use(express.static('public'));
 
 const data = [
-    { id: 1, name: "Qurbon" , lastname: "Sultonqulov", grade: "Front-end", age: "18 - years old",image: "http://localhost:3000/photo.jpg"},
-    { id: 2, image: "" },
-  ];
-  
+  { id: 1, name: "Qurbon", lastname: "Sultonqulov", grade: "Front-end", age: "18 - years old" },
+];
+
 app.get('/api/items', (req, res) => {
   res.json(data);
 });
 
+
 app.listen(port, () => {
-  console.log(`Server http://localhost:${port} da ishlayapti`);
+  console.log(`Server ${port}-portda ishlayapti`);
 });
