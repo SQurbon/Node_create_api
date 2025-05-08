556aa03dd5b71b938a81239221d3c27ff91e3e80
“SQurbon” <qurbonsultonqulov466@gmail.com>
2025-05-08 17:06:29 +0500

    ffff

--- server3.js
+++ server3.js
@@ -4,17 +4,24 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.static('public'));
+app.use(express.static('public')); // rasm uchun
 
 const data = [
-  { id: 1, name: "Qurbon", lastname: "Sultonqulov", grade: "Front-end", age: "18 - years old" },
+  {
+    id: 1,
+    name: "Ismim Akmal",
+    image: "https://your-api.onrender.com/photo.jpg"
+  },
+  {
+    id: 2,
+    name: "Bu test item"
+  }
 ];
 
 app.get('/api/items', (req, res) => {
   res.json(data);
 });
 
-
 app.listen(port, () => {
-  console.log(`Server ${port}-portda ishlayapti`);
+  console.log(`Server ishga tushdi: ${port}`);
 });
